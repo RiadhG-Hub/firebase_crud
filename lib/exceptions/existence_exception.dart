ad8093f5abab812cbce98eeda66574ae2f8e204f
riadhGharbi <gharbiriadh16@gmail.com>
2024-10-04 22:56:59 +0100

    add documentation

--- lib/exceptions/existence_exception.dart
+++
@@ -1,6 +0,0 @@
-class ExistenceException implements Exception {
-  final String exceptionValue;
-  ExistenceException({required this.exceptionValue});
-
-  String get exception => exceptionValue;
-}
